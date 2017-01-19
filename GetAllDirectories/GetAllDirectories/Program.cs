0647be00a8f42c9e7e6f9be9cc6788f1ac5c58c9
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-01-19 13:30:49 +0400

    Adding new solution

---
+++ GetAllDirectories/GetAllDirectories/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GetAllDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = Directory.GetDirectories(@"C:\");
+            Console.WriteLine("---- Directories of C: Disk----");
+
+            foreach (string name in array1)
+            {
+                Console.WriteLine(name);
+
+            }
+            Console.ReadKey();
+            Console.WriteLine("---- Files of C: Disk----");
+
+
+            string[] array2 = Directory.GetFiles(@"C:\");
+            foreach (string name in array2)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
