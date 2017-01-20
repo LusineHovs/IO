b0f70ca4206e10367f57cfdf5169cac13fd42c16
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-01-20 13:11:58 +0400

    Addition of some solutions

---
+++ DesktopFilesCleaning/DesktopFilesCleaning/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DesktopFilesCleaning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"C:\Users\L.Hovsepyan\Desktop\Testing");
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                if (!file.IsReadOnly)
+                {
+                    file.Delete();
+                }
+                else
+                {
+                    Console.WriteLine("Unfortunately not all files has been deleted in this directory");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
